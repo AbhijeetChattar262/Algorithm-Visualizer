891b0bfb93dab3c3eb586f94254037a18ec1ef1c
AbhijeetChattar262 <abchattar5503@gmail.com>
2024-09-19 15:30:36 +0530

    Landing page completed (Assets remaining)

--- Frontend/src/components/Landing/Hero.jsx
+++ Frontend/src/components/Landing/Hero.jsx
@@ -12,7 +12,7 @@ export default function Hero() {
             structures and algorithms unfold before your eyes along with AI
             chatbots to help clear your doubts
           </p>
-          <button className="start-btn">Get Started</button>
+          <button className="start-btn"><div className="start">Get Started</div><div className="arrow"><i class="fa-solid fa-arrow-right"></i></div></button>
         </div>
       </div>
     </div>
