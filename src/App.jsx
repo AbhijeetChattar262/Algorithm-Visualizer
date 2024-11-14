edd8404a8696d3abd0771ca72e76aeadce6b2d7c
AbhijeetChattar262 <abchattar5503@gmail.com>
2024-11-14 11:22:31 +0530

    Added user controls for pause, resume, step and reset

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,19 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { useParams } from 'react-router-dom';
 import Landing from './pages/Landing/Landing';
 import Signup from './pages/Signup/Signup';
 import Home from './pages/Home/Home';
-import Categories from './pages/Categories/Categories';
-import Category from './pages/Category/Category';
-import Algorithm from './pages/Algorithm/Algorithm';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route exact path="/" element={<Landing />} />
-        <Route path="/signup" element={<Signup />} />
         <Route path="/home" element={<Home />} />
-        <Route path="/categories" element={<Categories />} />
-        <Route path="/categories/:category" element={<CategoryWrapper />} />
-        <Route path="/categories/:category/:algorithm" element={<AlgorithmWrapper />} />
       </Routes>
     </BrowserRouter>
   );
 }
 
-function CategoryWrapper() {
-  const { category } = useParams();
-  return <Category subCategory={category} />;
-}
-
-function AlgorithmWrapper() {
-  const { algorithm } = useParams();
-  return <Algorithm algorithm={algorithm} />;
-}
 
 export default App;
