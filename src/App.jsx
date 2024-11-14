068e38a27d63ea6be589a279a8268be2101e7029
AbhijeetChattar262 <abchattar5503@gmail.com>
2024-11-14 22:11:02 +0530

    Optimised sorting algorithms

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,11 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Landing from './pages/Landing/Landing';
 import Signup from './pages/Signup/Signup';
 import Home from './pages/Home/Home';
+import { injectSpeedInsights } from '@vercel/speed-insights';
+
 
 function App() {
+  injectSpeedInsights();
   return (
     <BrowserRouter>
       <Routes>
